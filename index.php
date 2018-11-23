8bbdb6254559fa165401b0410dadcf73aaac49d3
Maneesh Parihar <maneeshparihar@gmail.com>
2018-11-24 02:18:27 +0530

    first commit

---
+++ index.php
@@ -0,0 +1,171 @@
+<?php ?>
+
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+  <meta charset="UTF-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <meta http-equiv="X-UA-Compatible" content="ie=edge">
+  <meta name="format-detection" content="telephone=no">
+  <title>Goldstar Services</title>
+  <link rel="stylesheet" type="text/css" href="assets/css/bootstrap.min.css">
+  <link rel="stylesheet" type="text/css" href="assets/css/gsvending.css">
+
+</head>
+<body class="bg-secondary">
+
+
+
+    <nav class="navbar navbar-expand-sm navbar-dark bg-dark">
+      <span class="py-2 px-3 rounded" style="background-color: #fff; max-width: 100%;"><img class="img-fluid" src="assets/img/Logo 5 Vector _transparent medium.png" ></span>
+
+      <a class="navbar-brand ml-5" href="#">Goldstar Services</a>
+
+      <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+        <span class="navbar-toggler-icon"></span>
+      </button>
+
+      <div class="collapse navbar-collapse" id="navbarSupportedContent">
+        <ul class="navbar-nav m-auto">
+          <li class="nav-item active px-3">
+            <a class="nav-link" href="/">Home <span class="sr-only">(current)</span></a>
+          </li>
+          <!-- <li class="nav-item">
+            <a class="nav-link" href="#">Products and Services</a>
+          </li> -->
+          <li class="nav-item dropdown px-3">
+            <a class="nav-link dropdown-toggle" href="#" id="navbarDropdown" role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
+              Products and Services
+            </a>
+            <div class="dropdown-menu" aria-labelledby="navbarDropdown">
+              <a class="dropdown-item" href="#">Tea Coffee Vending Machines</a>
+              <a class="dropdown-item" href="#">Water Dispensers</a>
+              <!-- <div class="dropdown-divider"></div> -->
+              <a class="dropdown-item" href="#">Business Services</a>
+            </div>
+          </li>
+          <li class="nav-item px-3">
+            <a class="nav-link" href="#">Contact Us <span class="sr-only">(current)</span></a>
+          </li>
+          <!-- <li class="nav-item">
+            <a class="nav-link disabled" href="#">Disabled</a>
+          </li> -->
+        </ul>
+        <!-- <form class="form-inline my-2 my-lg-0">
+          <input class="form-control mr-sm-2" type="search" placeholder="Search" aria-label="Search">
+          <button class="btn btn-outline-success my-2 my-sm-0" type="submit">Search</button>
+        </form> -->
+      </div>
+    </nav>
+
+
+
+
+
+
+
+<div class="container-fluid">
+
+  <div class="row">
+          <div class="d-sm-flex col-sm-12 col-md-9 py-4">
+                  <div id="carouselExampleIndicators" class="carousel slide " data-ride="carousel">
+                    <ol class="carousel-indicators">
+                      <li data-target="#carouselExampleIndicators" data-slide-to="0" class="active"></li>
+                      <li data-target="#carouselExampleIndicators" data-slide-to="1"></li>
+                      <li data-target="#carouselExampleIndicators" data-slide-to="2"></li>
+                    </ol>
+                    <div class="carousel-inner border-radius-2">
+                      <div class="carousel-item active">
+                        <img class="d-block w-100" src="assets/img/slide-image-1.jpg" alt="First slide">
+                            <div class="carousel-caption d-none d-md-block">
+                            <h5>...</h5>
+                            <p>vending</p>
+                            </div>
+                      </div>
+                      <div class="carousel-item">
+                        <img class="d-block w-100" src="assets/img/slide-image-2.jpg" alt="Second slide">
+                          <div class="carousel-caption d-none d-md-block">
+                          <h5>...</h5>
+                          <p>...</p>
+                          </div>
+                      </div>
+                      <div class="carousel-item ">
+                        <img class="d-block w-100" src="assets/img/slide-image-3.jpg" alt="Third slide">
+                            <div class="carousel-caption d-none d-md-block">
+                            <h5>...</h5>
+                            <p>...</p>
+                            </div>
+                      </div>
+                    </div>
+                    <a class="carousel-control-prev border-radius-2" href="#carouselExampleIndicators" role="button" data-slide="prev">
+                      <span class="carousel-control-prev-icon" aria-hidden="true"></span>
+                      <span class="sr-only">Previous</span>
+                    </a>
+                    <a class="carousel-control-next" href="#carouselExampleIndicators" role="button" data-slide="next">
+                      <span class="carousel-control-next-icon" aria-hidden="true"></span>
+                      <span class="sr-only">Next</span>
+                    </a>
+                  </div>
+          </div>
+
+          <div class="col-12 col-sm-12 col-md-3 d-flex flex-column justify-content-start px-4 px-sm-3 py-2">
+                <div class="display-4">
+                  Contact Us
+                </div>
+                <div class="mt-4">You can reach us on <br>
+                  <strong class="d-inline">Phone:</strong> +91-72176 56318 <br>
+                  <strong>Email:</strong> <br>
+                  info@goldstarservices.in <br>
+                  goldstar1808@gmail.com
+                </div>
+          </div>
+</div>
+
+
+<div class="container mx-2 mt-4 mt-md-2">
+        <!-- Example row of columns -->
+        <div class="row">
+          <div class="col-md-4 px-0 px-sm-2">
+            <h2>Tea Coffee Vending Machines</h2>
+            <p>Donec id elit non mi porta gravida at eget metus. Fusce dapibus, tellus ac cursus commodo, tortor mauris condimentum nibh, ut fermentum massa justo sit amet risus. Etiam porta sem malesuada magna mollis euismod. Donec sed odio dui. </p>
+            <p><a class="btn btn-dark" href="#" role="button">View details »</a></p>
+          </div>
+          <div class="col-md-4 px-0 px-sm-2">
+            <h2>Water Dispensers</h2>
+            <p>Donec id elit non mi porta gravida at eget metus. Fusce dapibus, tellus ac cursus commodo, tortor mauris condimentum nibh, ut fermentum massa justo sit amet risus. Etiam porta sem malesuada magna mollis euismod. Donec sed odio dui. </p>
+            <p><a class="btn btn-dark" href="#" role="button">View details »</a></p>
+          </div>
+          <div class="col-md-4 px-0 px-sm-2">
+            <h2>Business Services</h2>
+            <p>Donec sed odio dui. Cras justo odio, dapibus ac facilisis in, egestas eget quam. Vestibulum id ligula porta felis euismod semper. Fusce dapibus, tellus ac cursus commodo, tortor mauris condimentum nibh, ut fermentum massa justo sit amet risus.</p>
+            <p><a class="btn btn-dark" href="#" role="button">View details »</a></p>
+          </div>
+        </div>
+
+        <hr>
+
+      </div>
+
+
+<!--Start of Tawk.to Script-->
+<script type="text/javascript">
+var Tawk_API=Tawk_API||{}, Tawk_LoadStart=new Date();
+(function(){
+var s1=document.createElement("script"),s0=document.getElementsByTagName("script")[0];
+s1.async=true;
+s1.src='https://embed.tawk.to/5af439175f7cdf4f0534080c/default';
+s1.charset='UTF-8';
+s1.setAttribute('crossorigin','*');
+s0.parentNode.insertBefore(s1,s0);
+})();
+</script>
+<!--End of Tawk.to Script-->
+
+
+<script type="text/javascript" src="assets/js/jquery-3.2.1.min.js"> </script>
+<script type="text/javascript" src="assets/js/bootstrap.bundle.min.js"> </script>
+<script type="text/javascript" src="assets/js/gsvending.js"> </script>
+
+</body>
+</html>
